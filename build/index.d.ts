56d8f28540466eb72bd8f84e041aa3b9db1b8b30
lucas.j <lucas.j@hpcnt.com>
2021-08-14 17:50:11 +0900

    init tsconfig json package

--- build/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare type ShadowType = "hard";
-interface Option {
-    shadow_type: ShadowType;
-    padding?: boolean;
-}
-declare function shadowizard(option: Option): void;
-export default shadowizard;
